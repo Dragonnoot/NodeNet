cc29b0fcf576652660e7eb5f882344742a0fc619
Emmie Jones, Dragonnoot <affordablehaulingandtransport@gmail.com>
2021-06-24 16:46:37 -0400

    Add better error messages and minor security to RGBNode (↓)
    This iteration's RGBNode will not work! That will be fixed later.

--- NodeNet/Nodes/RGBNode/gateLogic.js
+++ NodeNet/Nodes/RGBNode/gateLogic.js
@@ -1,70 +1,103 @@
-const field = document.getElementById('flexibleSpace');
+var codeHasRun = 0;
 
-function logField(fieldData) {
-  field.innerHTML = fieldData;
-}
+function doEverything() {
 
-logField("[1]> Initializing...");
+  if(codeHasRun) {return}
 
-//        ↓  ↓ ██ Will be Sourced from Server ██  ↓  ↓
+  codeHasRun += 1;
 
-let fullData = {QOPAL: {userName: "GeoDrake", signed: true, accountAge: 0}, POOPOO: {userName: "yeeHaw", signed: false, accountAge: 0}}; // <-- note that this data will be confined to the back end. STILL NEED LOGIN SYSTEM.
+  const field = document.getElementById('flexibleSpace');
 
-//        ↓  ↓ ██            Local            ██  ↓  ↓
- 
-let userData = {userName: "GeoDrake", userID: "QOPAL", signed: false};
-let signUpdated = false;
+  function logField(fieldData) {
+    field.innerHTML = fieldData;
+  }
 
-// Make sure signed field is accurate:
+  logField("[1]> Initializing...");
 
-function updateSigned(userUserName, userUserID) {
+  //        ↓  ↓ ██ Will be Sourced from Server ██  ↓  ↓
 
-  logField("[2-1]> Updating Sign Status...");
+  let fullData = {QOPAL: {userName: "GeoDrake", signed: 0, accountAge: 0}, POOPOO: {userName: "yeeHaw", signed: false, accountAge: 0}}; // <-- note that this data will be confined to the back end. STILL NEED LOGIN SYSTEM.
 
-  let innerFullData = fullData[userUserID];
+  // ↑↑▬ NOT VISIBLE TO USER. IF THIS IS VISIBLE THEN SECURITY IS GONE. ▬↑↑
+
+  //        ↓  ↓ ██            Local            ██  ↓  ↓
   
-  if (innerFullData.userName == userUserName) {
-    userData.signed = innerFullData.signed;
+  let userData = {userName: "GeoDrake", userID: "QOPAL", signed: false};
+  let signUpdated = false;
+
+  // Make sure signed field is accurate:
+
+  function updateSigned(userUserName, userUserID) {
+
+    logField("[2-1]> Updating Sign Status...");
+
+    let innerFullData = fullData[userUserID];
+    codeHasRun += 1;
+    
+    if (innerFullData.userName == userUserName) {
+      userData.signed = innerFullData.signed;
+    }
+    else {
+      userData.signed = false;
+      logField("There was an error! Your recorded username, " +  userUserName + ", does not match your UserID." + "[Error occured at 2-1, supposed to transition to 2-2]");
+      alert("Sorry, there was a bad error! Report: Sent username: " + userUserName + ", expected " + innerFullData.userName);
+    }
+
+    logField("[2-2]> Noting update of sign...");
+    signUpdated = true;
+
   }
-  else {
-    userData.signed = false;
-    logField("There was an error! Your recorded username, " +  userUserName + ", does not match your UserID." + "[Error occured at 2-1, supposed to transition to 2-2]");
-    alert("Sorry, there was a bad error! Report: Sent username: " + userUserName + ", expected " + innerFullData.userName);
+  function findUserData() {
+    codehasRun += 1;
   }
 
-  logField("[2-2]> Noting update of sign...");
-  signUpdated = true;
+  updateSigned(userData.userName, userData.userID);
 
+  // Check that user is signed:
+
+  function isSigned() {
+
+    logField("[3-1]> Checking that you're signed...")
+
+    if(userData.signed == true) {
+      logField("[3-2]> You're signed! Showing Connect button...");
+      return true;
+    }
+    else {
+      logField("There was an error! You either aren't signed or haven't been updated. Will try again! [Error occured at 3-1, supposed to transition to 3-2]");
+      alert("Sorry, there was a bad error! Report: Must be signed. Sign status: " + userData.signed + ", Updated: " + signUpdated);
+
+      updateSigned(userData.userName, userData.userID);
+      findUserData();
+
+      if(userData.signed == true) {
+        logField("[3-2]> You're signed! Showing Connect button...");
+        return true;
+      }
+      else {
+        logField("You still either aren't signed properly or can't be updated, but we've already tried again to update your status.  [Error occured at 3-1, supposed to transition to 3-2 and already retried updating]");
+        alert("Sorry, there was a bad error! Report: Must be signed. Sign status: " + userData.signed + ", Updated: " + signUpdated);
+        return false;
+      }
+    }
+  }
+
+  if (isSigned()) {
+    logField("<button id='connectButton'>Connect</button>"); 
+  }
+
+  const connectButton = document.getElementById('connectButton');
+
+  connectButton.addEventListener('click', function() {
+    var userUserID = userData.userID;
+    var innerUserData = fullData[userUserID];
+
+    innerUserData.accountAge = 1; //Would send this data to server to be retrieved by main.html
+    window.open("./main.html", "_self");
+  });
+
+  codeHasRun += 1;
 }
-
-updateSigned(userData.userName, userData.userID);
-
-// Check that user is signed:
-
-function isSigned() {
-
-  logField("[3-1]> Checking that you're signed...")
-
-  if(userData.signed == true) {
-    logField("[3-2]> You're signed! Showing Connect button...");
-    return true;
-  }
-  else {
-    logField("There was an error! You aren't signed. [Error occured at 3-1, supposed to transition to 3-2]");
-    alert("Sorry, there was a bad error! Report: Must be signed. Sign status: " + userData.signed + ", Updated: " + signUpdated);
-    return false;
-  }
-}
-
-if (isSigned()) {
-  logField("<button id='connectButton'>Connect</button>"); 
-}
-
-const connectButton = document.getElementById('connectButton');
-
-connectButton.addEventListener('click', function() {
-  var userUserID = userData.userID;
-  var innerUserData = fullData[userUserID];
 
   innerUserData.accountAge = 1; //Would send this data to server to be retrieved by main.html
   window.open("./main.html", "_self");
