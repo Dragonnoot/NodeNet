1f59f524a41eb2fe8227dac00ae1802c67320bcc
Emmie Jones, Dragonnoot <affordablehaulingandtransport@gmail.com>
2021-07-28 18:38:43 -0400

    thingy I forgor

--- NodeNet/Nodes/RGBNode/gateLogic.js
+++ NodeNet/Nodes/RGBNode/gateLogic.js
@@ -1,5 +1,33 @@
 var codeHasRun = 0;
 
+var fs = require("fs");
+
+// Open a new file with name input.txt and write Simply Easy Learning! to it.
+
+fs.writeFile('input.txt', 'crunchy (:', function(err) {
+
+   if (err) {
+
+      return console.error(err);
+
+   }
+
+   // Read the newly written file and print all of its content on the console
+   fs.readFile('input.txt', function (err, data) {
+
+      if (err) {
+
+         return console.error(err);
+
+      }
+      else {
+        alert(data);
+      }
+
+   });
+
+});
+
 function doEverything() {
 
   if(codeHasRun) {return}
